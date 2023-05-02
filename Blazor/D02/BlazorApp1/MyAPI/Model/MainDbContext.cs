a22cd4cc8eec2f63e9f7fdd63e6aad4de1eca3ef
ReemRaed <reemtaha998@gmail.com>
2023-05-02 11:32:00 +0300

    D02Blazor

---
+++ Blazor/D02/BlazorApp1/MyAPI/Model/MainDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedLibrary;
+
+namespace MyAPI.Model
+{
+    public class MainDbContext :DbContext
+    {
+        public MainDbContext(DbContextOptions<MainDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Trainee> Trainees { get; set; }
+        public virtual DbSet<Track> Tracks { get; set; }
+    }
+}
